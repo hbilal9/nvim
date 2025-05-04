e9c52d5a69babbb73ae25ddb650565d89e95872b
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2025-05-04 13:52:18 +0500

    fix: update mappings.lua to disable spacebar behavior and improve save command mapping 🔧

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,13 +3,21 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- disable the spacebar default behavior in Normal and Visual mode
+map({ "n", "v" }, "<Space>", "<Nop>", opts)
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Explorer" })
+
+
+-- Save file with Cmd + S
+map("n", "<C-s>", ":w<CR>", { desc = "Save File" })
 
 -- Copilot Mappings
 map("n", "<leader>zc", ":CopilotChat<CR>", { desc = "Copilot Chat Toggle" })
